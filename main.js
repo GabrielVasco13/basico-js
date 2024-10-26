cdcb0e82aaa9db1bd6a7b00f3fc4662b2905b50b
Gabriel FV <gabrielvasco906@gmail.com>
2024-10-26 12:23:46 -0300

    função de diminuir o valor funfano

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function buttonClick() {
     numero++;
     numeroElement.textContent = numero;
 }
+
+function decrement() {
+    const numeroElement = document.querySelector('#numero');
+    let numero = Number(numeroElement.textContent);
+    numero--;
+    numeroElement.textContent = numero;
+}
